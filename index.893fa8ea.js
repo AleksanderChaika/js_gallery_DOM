68f59b99989d5e6f50bd830fbc31ca09b460c134
Aleksander Chaika <alekschayka97@gmail.com>
2025-10-04 15:00:13 +0300

    make build

---
+++ index.893fa8ea.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg");document.querySelector("#thumbs").addEventListener("click",function(t){var r,a=t.target;"IMG"===a.tagName?r=a.closest("a"):"A"===a.tagName&&(r=a),r&&r.href&&(t.preventDefault(),e.src=r.href)});
+//# sourceMappingURL=index.893fa8ea.js.map
